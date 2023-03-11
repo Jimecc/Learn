d7c6d4e5d517c2098b700e79a15164d84b89ca60
Jimecc <41030702+Jimecc@users.noreply.github.com>
2023-03-11 11:24:18 +0800

    Merge Vue

---
+++ Vue/firstdemo_baidu/README.md
@@ -0,0 +1,20 @@
+# Baidu.com
+<div align=center><img src="https://files.catbox.moe/ltfc1r.png" style="width:90%"></div>
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ Vue/firstdemo_baidu/src/main.js
@@ -0,0 +1,28 @@
+import router from './router'
+import store from './store'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: function (h) { return h(App) }
+}).$mount('#app')
+
+
+
+
+import {postRequest} from "./utils/api";
+import {getRequest} from "./utils/api";
+import {putRequest} from "./utils/api";
+import {deleteRequest} from "./utils/api";
+
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
